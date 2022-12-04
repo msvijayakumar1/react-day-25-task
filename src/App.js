4634f39f921dd2ed993852d3e936aa054c042682
Vijayakumar <msvijayakumar1>
2022-12-04 02:07:09 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./css/sb-admin-2.css";
 import Dashboard from './Dashboard';
 import "./css/fontawesome-free/css/all.min.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import User from './User';
 
 function App() {
   return (
+    <BrowserRouter>
     <div id= "wrapper">
        <Sidebar/>
        <div id="content-wrapper" class="d-flex flex-column">
        <div id="content">
         <Topbar/>
-        <Dashboard/>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/users' element={<User/>}/>
+        </Routes>
         </div>
        </div>
       
     </div>
+    </BrowserRouter>
   );
 }
 
